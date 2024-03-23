106c725079bfce5f834068fa4a539fdc9af83e63
Anastasia-Kostenko <139360670+Anastasia-Kostenko@users.noreply.github.com>
2024-03-23 13:46:32 +0200

    commit-hw-04

---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,16 @@
+import css from './ImageCard.module.css';
+
+export const ImageCard = ({ item }) => {
+  return (
+    <div className={css.wrapperImg}>
+      <div>
+        <img
+          className={css.img}
+          src={item.urls.small}
+          alt={item.alt_description}
+          data-id={item.id}
+        />
+      </div>
+    </div>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
-import 'modern-normalize';
+import { App } from './components/App';
 import './index.css';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
