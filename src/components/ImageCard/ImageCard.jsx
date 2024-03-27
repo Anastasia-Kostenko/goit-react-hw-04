a524197501ac17b26adc0d68a7225d1464e40b9a
Anastasia-Kostenko <139360670+Anastasia-Kostenko@users.noreply.github.com>
2024-03-27 20:21:43 +0200

    commit-onClick

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,21 +1,15 @@
 import css from './ImageCard.module.css';
 
-export const ImageCard = ({ item }) => {
-  const handleClick = () => {
-    console.log('Image clicked:', item.id);
-  };
-
+export const ImageCard = ({ item, onClickModal }) => {
   return (
     <div className={css.wrapperImg}>
-      <div>
-        <img
-          className={css.img}
-          src={item.urls.small}
-          alt={item.alt_description}
-          data-id={item.id}
-          onClick={handleClick}
-        />
-      </div>
+      <img
+        className={css.img}
+        src={item.urls.small}
+        alt={item.alt_description}
+        data-id={item.id}
+        onClick={() => onClickModal(item.id)}
+      />
     </div>
   );
 };
